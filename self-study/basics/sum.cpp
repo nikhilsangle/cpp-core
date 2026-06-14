8a859299b1688ba7d4610c9e57bf22505e187b05
nikhilsangle <nikhilsangle@gmail.com>
2026-06-14 14:48:27 +0530

    Add sum of two numbers program.

---
+++ self-study/basics/sum.cpp
@@ -0,0 +1,18 @@
+// Addition of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+    cout << "Enter number a: ";
+    cin >> a;
+    cout << "Enter number b: ";
+    cin >> b;
+    sum = a + b;
+    cout << "Sum of " << a << " & " << b << " is: " << sum;
+    return 0;
+}
